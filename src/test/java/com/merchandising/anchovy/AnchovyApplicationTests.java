29f07a6bb35f53fcba47bf95d161cee86a2a38bb
jinsoo89 <hello.python89@gmail.com>
2021-08-22 00:31:28 +0900

    Initialize Spring Boot project

---
+++ src/test/java/com/merchandising/anchovy/AnchovyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merchandising.anchovy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnchovyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
